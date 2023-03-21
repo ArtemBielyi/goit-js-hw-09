569eca40d4649eee1c85163aa65ca4c8426e3996
ArtemBielyi <kolhozreshaet@gmail.com>
2023-03-21 20:18:02 +0200

    додає бібліотеки

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,45 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const startButton = document.querySelector('button[data-start]');
+// const stopButton = document.querySelector('button[data-stop]');
+// const bodyEl = document.querySelector('body');
+
+// startButton.addEventListener('click', () => {});
+
+// const timerEl = setInterval(changeColor, 1000);
+
+// function changeColor() {
+//   const randomColor = getRandomHexColor();
+//   bodyEl.style.backgroundColor = randomColor;
+// }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+let intervalId;
+
+function startColorChange() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
