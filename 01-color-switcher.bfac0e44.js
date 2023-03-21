6aa7ac6c561ec4320c0f14d800b9bb7c1340c6c9
ArtemBielyi <114308636+ArtemBielyi@users.noreply.github.com>
2023-03-21 18:18:36 +0000

    Deploying to gh-pages from @ ArtemBielyi/goit-js-hw-09@569eca40d4649eee1c85163aa65ca4c8426e3996 🚀

---
+++ 01-color-switcher.bfac0e44.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.bfac0e44.js.map
