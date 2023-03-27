600b6ec4a623b9e615573304b1ba8aca3b615afd
ArtemBielyi <kolhozreshaet@gmail.com>
2023-03-27 15:36:11 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// const startButton = document.querySelector('button[data-start]');
-// const stopButton = document.querySelector('button[data-stop]');
-// const bodyEl = document.querySelector('body');
-
-// startButton.addEventListener('click', () => {});
-
-// const timerEl = setInterval(changeColor, 1000);
-
-// function changeColor() {
-//   const randomColor = getRandomHexColor();
-//   bodyEl.style.backgroundColor = randomColor;
-// }
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
